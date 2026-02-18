864f2a2aa1fae206ad975993f7550c237717edf8
werto87 <huhulasmichinruhe@web.de>
2026-02-18 20:49:07 +0530

    dep version bump

--- all/conanfile.py
+++ all/conanfile.py
@@ -38,8 +38,8 @@ class ModernDurakUnrealCxx(ConanFile):
         self.requires("login_matchmaking_game_shared/latest",transitive_headers=True)
         self.requires("modern_durak_game_option/latest",transitive_headers=True)        
         self.requires("modern_durak_game_shared/latest",transitive_headers=True)
-        self.requires("matchmaking_proxy/4.2.12",transitive_headers=True)
-        self.requires("modern_durak_game/6.6.7",transitive_headers=True)
+        self.requires("matchmaking_proxy/4.3.0",transitive_headers=True)
+        self.requires("modern_durak_game/6.7.0",transitive_headers=True)
         self.requires("durak/2.1.1",transitive_headers=True)
 
     def source(self):
